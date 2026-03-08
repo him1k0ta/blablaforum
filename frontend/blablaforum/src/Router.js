bfdf47ecc9590583c813ecb314ab210b8a5d4055
him1k0ta <149035975+him1k0ta@users.noreply.github.com>
2026-03-09 04:54:41 +0700

    feat: реализована маршрутизация (frontend)
    - Добавлена настройка React Router
    - Настроены пути для основных страниц
    - Создана структура навигации

---
+++ frontend/blablaforum/src/Router.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Route, Routes} from 'react-router-dom';
+
+import Main from './pages/main';
+import Auth from './pages/auth';
+import Registration from './pages/registration';
+import Threads from './pages/threads';
+import Rules from './pages/rules';
+import About from './pages/about';
+import Create from './pages/create';
+import AdminPanel from './pages/admin';
+import ThreadPage from './pages/threadpage';
+const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Main/>}/>
+            <Route path="/auth" element={<Auth/>}/>
+            <Route path="/register" element={<Registration/>}/>
+            <Route path="/threads" element={<Threads/>}/>
+            <Route path="/rules" element={<Rules/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/create" element={<Create/>}/>
+            <Route path="/admin" element={<AdminPanel />} />
+            <Route path="/thread/:id" element={<ThreadPage />} />
+        </Routes>
+    );
+};
+
+export default AppRoutes;
